447625dc8d0629cef25d6a75ed37117b79a19447
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-19 12:18:36 +0500

    test

---
+++ condition/variable.tf
@@ -0,0 +1,24 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "environment" {}
+
+
+variable "instance_count" {
+  type    = number
+  default = 1
+}
+
+variable "instance_type" {
+  default = {
+  "us-east-1.prod" = "t2.micro"
+  "us-east-1.stag" = "t3.micro"
+  }
+}
+
+variable "ami" {
+  default = {
+    "us-east-1.prod" = "ami-0f05e5c9509a223df" #"ami-0574da719dca65348"
+    "us-east-1.stag" = "ami-0f05e5c9509a223dd" #"ami-0574da719dca65348"
+  }
+}
