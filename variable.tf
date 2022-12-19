471c9479fe24adf0bcc7d7f4c76ad97116fdf0bd
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-19 13:05:52 +0500

    asd

--- variable.tf
+++ variable.tf
@@ -14,21 +14,21 @@ variable "environment" {
 
 variable "instance_count" {
 default = {
-  "us-west-1.prod" = "1"
-  "us-west-1.stag" = "0"
+  "us-west-1.prod" = "0"
+  "us-west-1.staging" = "1"
 }
 }
 
 variable "instance_type" {
   default = {
   "us-west-1.prod" = "t2.micro"
-  "us-west-1.stag" = "t3.micro"
+  "us-west-1.staging" = "t3.micro"
   }
 }
 
 variable "ami" {
   default = {
     "us-west-1.prod" = "ami-0f05e5c9509a223df" #"ami-0574da719dca65348"
-    "us-west-1.stag" = "ami-0f05e5c9509a223dd" #"ami-0574da719dca65348"
+    "us-west-1.staging" = "ami-0f05e5c9509a223df" #"ami-0574da719dca65348"
   }
 }
