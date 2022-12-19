6858b4b092bc004bf154355958b4228a366f797c
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-19 12:49:42 +0500

    asd

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "redhat-instance" {
-  count                       = var.instance_count
+  count                       = var.instance_count["${var.region}.${var.environment}"]
   ami                         = var.ami["${var.region}.${var.environment}"]
   instance_type                = var.instance_type["${var.region}.${var.environment}"]
 }
--- variable.tf
+++ variable.tf
@@ -13,20 +13,22 @@ variable "environment" {
 
 
 variable "instance_count" {
-  type    = number
-  default = 1
+default = {
+  "us-west-1.prod" = "1"
+  "us-west-1.stag" = "0"
+}
 }
 
 variable "instance_type" {
   default = {
-  "us-east-1.prod" = "t2.micro"
-  "us-east-1.stag" = "t3.micro"
+  "us-west-1.prod" = "t2.micro"
+  "us-west-1.stag" = "t3.micro"
   }
 }
 
 variable "ami" {
   default = {
-    "us-east-1.prod" = "ami-0f05e5c9509a223df" #"ami-0574da719dca65348"
-    "us-east-1.stag" = "ami-0f05e5c9509a223dd" #"ami-0574da719dca65348"
+    "us-west-1.prod" = "ami-0f05e5c9509a223df" #"ami-0574da719dca65348"
+    "us-west-1.stag" = "ami-0f05e5c9509a223dd" #"ami-0574da719dca65348"
   }
 }
