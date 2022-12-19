f94c76f90385750af4730acd2da282dc2c1cc20a
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-19 12:54:32 +0500

    asd

--- instance.tf
+++ instance.tf
@@ -3,4 +3,9 @@ resource "aws_instance" "redhat-instance" {
   count                       = var.instance_count["${var.region}.${var.environment}"]
   ami                         = var.ami["${var.region}.${var.environment}"]
   instance_type                = var.instance_type["${var.region}.${var.environment}"]
+  tags = {
+    "Name" = "Ubunt-vm"
+    "Creator" = "Terraform-code"
+    "repo" = "https://github.com/rizwannadeem2017/terraform-prod.git"
+  }
 }
