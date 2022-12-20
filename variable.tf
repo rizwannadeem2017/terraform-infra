ae2c7b26dd9860446d129026ae85c39eb2dafbbb
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-20 21:58:39 +0500

    asdad

--- variable.tf
+++ variable.tf
@@ -14,8 +14,8 @@ variable "tags" {
 
 variable "instance_count" {
   default = {
-    "us-west-1.prod"    = "3"
-    "us-west-1.staging" = "5"
+    "us-west-1.prod"    = "0"
+    "us-west-1.staging" = "0"
   }
 }
 
