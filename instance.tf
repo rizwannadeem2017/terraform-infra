9e8232cfa9b7f72d71531a27c69b70bbd67e0654
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-20 13:26:40 +0500

    test

--- instance.tf
+++ instance.tf
@@ -1,11 +1,11 @@
 
 module "redhat-vm" {
-  source = "github.com/rizwannadeem2017/terraform-modules//test?ref=v10.1.2"
+  source = "github.com/rizwannadeem2017/terraform-modules//test"
 
   environment = var.environment
   instance_count = var.instance_count["${var.region}.${var.environment}"]
   ami           = var.ami["${var.region}.${var.environment}"]
   instance_type = var.instance_type["${var.region}.${var.environment}"]
-  tags          = merge(map("Name", "ubuntu-vm-${format("%02d", count.index + 1)}"), var.tags)
+  tags          = var.tags
 
 }
--- variable.tf
+++ variable.tf
@@ -21,8 +21,8 @@ variable "tags" {
 
 variable "instance_count" {
   default = {
-    "us-west-1.prod"    = "1"
-    "us-west-1.staging" = "0"
+    "us-west-1.prod"    = "2"
+    "us-west-1.staging" = "3"
   }
 }
 
