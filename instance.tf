d93000f6f3e1e7350366fdd6cc0e43679f0bf197
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-20 16:06:14 +0500

    asdad

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ module "redhat-vm" {
   source = "github.com/rizwannadeem2017/terraform-modules//test"
  # source = "/Users/rizwannadeem/Desktop/upwork/rizwan-terraform/terraform-modules/test"
 
-  environment = var.environment
+  #environment = var.environment
   instance_count = var.instance_count["${var.region}.${var.environment}"]
   ami           = var.ami["${var.region}.${var.environment}"]
   instance_type = var.instance_type["${var.region}.${var.environment}"]
