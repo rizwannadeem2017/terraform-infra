1efbd5bbc1df3c2e56b653b46609e70344711204
Rizwan Nadeem <rizwannadeem@cloud.upwork.com>
2023-02-14 12:52:54 +0500

    remove instances

--- instance-variable.tf
+++ instance-variable.tf
@@ -12,8 +12,8 @@ variable "tags" {
 
 variable "instance_count" {
   default = {
-    "us-west-1.prod"    = "1"
-    "us-west-1.staging" = "1"
+    "us-west-1.prod"    = "0"
+    "us-west-1.staging" = "0"
   }
 }
 
